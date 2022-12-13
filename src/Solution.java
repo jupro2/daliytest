a355aaf085a5a9f32913e4e6a855e1e091bf5496
29144 <2914411787@qq.com>
2022-12-13 08:54:00 +0800

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int m = 0;//return value
+        int min=Integer.MIN_VALUE;//min value  -2^31
+        int max= Integer.MAX_VALUE;//max value   2^31-1
+       while (x!=0)
+       {
+           if(m<min/10||m>max/10)//判断边界问题！！！！！
+           {
+               return 0;
+           }
+           int val=x%10;
+           m= m*10+val;
+           x/=10;
+       }
+        return  m;
+    }
+}
